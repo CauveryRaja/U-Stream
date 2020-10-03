a508694d0f18bb23ecaa83692c84d9c409e7d9ff
Raja <63710787+CauveryRaja@users.noreply.github.com>
2020-10-03 09:41:39 +0530

    Removed MongoDB Atlas user credentials

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dbURI: 'mongodb+srv://cauveryraja:raja2533@cluster0.dtrbw.gcp.mongodb.net/u-stream-db?retryWrites=true&w=majority'
+    dbURI: 'mongodb+srv://username:password@cluster0.dtrbw.gcp.mongodb.net/u-stream-db?retryWrites=true&w=majority'
 }
