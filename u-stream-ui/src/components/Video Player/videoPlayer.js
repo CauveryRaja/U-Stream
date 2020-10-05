26817190745c70ec1f68014d2a5d70d5a5e227fa
CauveryRaja <cauveryraja25@gmail.com>
2020-10-05 20:00:32 +0530

    Created Video Player component and injected in App component

---
+++ u-stream-ui/src/components/Video Player/videoPlayer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <section className="player">
+        Play Video
+        <video width="600" controls>
+          <source src="../../../public/defaultVideo.mp4" type="video/mp4"></source>
+          Your browser does not support Video element
+        </video>
+      </section>
+    );
+  }
+}
+
+export default VideoPlayer;
