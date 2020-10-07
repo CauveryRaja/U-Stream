bd3e93385bc3b640ea382658202eb20c7691180a
CauveryRaja <cauveryraja25@gmail.com>
2020-10-07 19:07:51 +0530

    Used Concurrently to run both Client and Server at the same time

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use('/', function(req, res) {
     res.send('<h2>Welcome to Express JS</h2>');
 });
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- u-stream-ui/src/App.js
+++ u-stream-ui/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import VideoPlayer from './components/Video Player/videoPlayer';
 
