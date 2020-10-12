e63a99b012943a1b13d391e70273ae2e82a9b614
CauveryRaja <cauveryraja25@gmail.com>
2020-10-12 20:38:52 +0530

    Created Video Form component that allows perform Create/Update operations

---
+++ u-stream-ui/src/components/Video Form/videoForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class VideoForm extends Component {
+    state = {
+      title: '',
+      description: '',
+      author: '',
+      category: '',
+      content: ''
+    };
+
+    onFormSubmit() {
+      console.log('Form submitted...');
+    }
+
+    render() {
+      return (
+        <div className="video-form-container">
+          <form onSubmit={onFormSubmit}>
+            <div className="form-group">
+              <label>Title</label>
+              <input type="text" name="title"/>
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <input type="text" name="description"/>
+            </div>
+            <div className="form-group">
+              <label>Category</label>
+              <input type="text" name="category"/>
+            </div>
+            <div className="form-group">
+              <label>Author</label>
+              <input type="text" name="author"/>
+            </div>
+            <div className="form-group">
+              <label>Content</label>
+              <input type="file" name="content"/>
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
+      )
+    }
+
+}
